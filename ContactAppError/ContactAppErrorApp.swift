82add0b8227d003839b4a486d8ff489161d82081
PhungMHieu <kiritosad41@gmail.com>
2025-09-03 23:20:16 +0700

    Initial Commit

---
+++ ContactAppError/ContactAppErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppErrorApp.swift
+//  ContactAppError
+//
+//  Created by Admin on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactAppError/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactAppError
+//
+//  Created by Admin on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
